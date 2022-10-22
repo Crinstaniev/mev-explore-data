b9c48f8b6f3cbe132ab0745b3112b28ac18269a3
Crinstaniev <zhuangzesen@gmail.com>
2022-10-23 00:31:39 +0800

    add comparison plot

--- make_plot.py
+++ make_plot.py
@@ -98,9 +98,82 @@ fig.update_layout(dict(
     height=600,
 ))
 
-fig.write_image('figures/svg/percent_of_total_block_gas_used_by_mev_transactions.svg')
+fig.write_image(
+    'figures/svg/percent_of_total_block_gas_used_by_mev_transactions.svg')
 fig.write_image(
     'figures/png/percent_of_total_block_gas_used_by_mev_transactions.png', scale=2)
+
+fig.show()
+
+# %%
+# Twitter and Google Trend Data
+data_trend = pd.read_csv(
+    'data/trend_volume.csv').drop('Unnamed: 0', axis=1)
+
+data_trend.columns = ['date', 'google_trend', 'tweet_volume', 'tweet_len']
+
+data_tweet_volume = data_trend[['date', 'tweet_volume']].query(
+    'tweet_volume > 0')
+data_google_trend = data_trend[['date', 'google_trend']].query(
+    'google_trend > 0')
+# %%
+fig = make_subplots()
+
+fig.add_trace(go.Line(
+    name='Google Trend',
+    x=data_google_trend['date'],
+    y=data_google_trend['google_trend'],
+    yaxis='y2',
+    opacity=.8
+))
+
+fig.add_trace(go.Line(
+    name='Tweets Volume',
+    x=data_tweet_volume['date'],
+    y=data_tweet_volume['tweet_volume'],
+    yaxis='y3',
+    opacity=.8
+))
+
+fig.add_trace(go.Line(
+    name='Gross Profit',
+    x=data_gp['time'],
+    y=data_gp['value'],
+    yaxis='y1'
+))
+
+fig.update_layout(dict(
+    template='ggplot2',
+    title='Daily Extracted MEV - Gross Profit v.s. Google Trend and Twitter Volume',
+    width=1000,
+    height=600,
+    xaxis=dict(
+        domain=[0, 0.8],
+        range=['2020-01-01', '2022-10-01']
+    ),
+    yaxis=dict(
+        title="Gross Profit",
+        side='left'
+    ),
+    yaxis2=dict(
+        title="Google Trend",
+        anchor="x",
+        overlaying="y",
+        side="right"
+    ),
+    yaxis3=dict(
+        title="Tweets Volume",
+        anchor="free",
+        overlaying="y",
+        side="right",
+        position=0.9
+    )
+))
+
+fig.write_image(
+    'figures/svg/mev_daily_gross_profit_vs_trend.svg')
+fig.write_image(
+    'figures/png/mev_daily_gross_profit_vs_trend.png', scale=2)
 
 fig.show()
 
